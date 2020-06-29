d3491fc04945c35b7474f3dfa2abc2f456aa0933
CzerwonyV <victor.veyrier@icloud.com>
2020-06-30 00:02:55 +0200

    Add button with img for 'Mon actu'

---
+++ src/components/Btn/BtnActu.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "reactstrap";
+import './Btn.css';
+
+const BtnActu = () => {
+  return (
+    <div className="BtnActuContent">
+      <div className="CercleBtn">
+        <Button color="link" className="BtnWild">
+          <a
+            href="https://twitter.com/CzerwonyV"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            <img src="https://user-images.githubusercontent.com/57908921/85471152-54374000-b5b0-11ea-9950-ca2b981eabac.png" alt="BtnWild" className="ImgWild" />
+          </a>
+        </Button>
+      </div>
+      <div className="TitleActuContent">
+        <h4 className="TitleActu">Mon actu:</h4>
+      </div>
+    </div>
+  );
+};
+
+export default BtnActu;
