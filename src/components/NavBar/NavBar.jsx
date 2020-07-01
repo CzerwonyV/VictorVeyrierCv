d4e6ae38c7dd86b5f142b06cc9690c1e20ed9abf
CzerwonyV <victor.veyrier@icloud.com>
2020-07-01 23:03:13 +0200

    feat(navbar): remove react router

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,15 +9,12 @@ import {
   NavLink,
   NavbarText
 } from "reactstrap";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Portfolio from "../PortFolio/Portfolio";
-import "./NavBar.css";
-import CV from "../CV/CV";
-import ContactMobile from "../Contact/ContactMobile";
+import { Link } from "react-router-dom";
 import ContactDestock from "../Contact/ContactDestock";
 import BtnGithub from "../Btn/BtnGithub";
 import BtnLinkedin from "../Btn/BtnLinkedin";
 import BtnTwitter from "../Btn/BtnTwitter";
+import "./NavBar.css";
 
 const NavBarCV = props => {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,72 +23,59 @@ const NavBarCV = props => {
 
   return (
     <div className="NavBarBack">
-      <Router>
-        <Navbar className="NavBarContent" light expand="md">
-          <NavbarBrand>
-            <Link className="linkTitleNavBar" to="/">
-              Victor Veyrier
-            </Link>
-          </NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              <NavItem>
-                <NavLink>
-                  <Link className="linkNavBar" to="/CV">
-                    CV
-                  </Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink id="ContactMobil">
-                  <Link className="linkNavBar" to="/Contact">
-                    Contact
-                  </Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink id="ContactDestock"  className="linkNavBar">
-                <ContactDestock />
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>
-                  <a
-                    className="linkNavBar"
-                    href="https://github.com/CzerwonyV/CzerwonyV/files/4765663/VictorVeyrier.pdf"
-                    download
-                  >
-                    <p className="linkNavBarPDFDestock">Télécharger mon CV</p>
-                    <p className="linkNavBarPDFMobile">CV en format PDF</p>
-                  </a>
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <NavItem className="IconNav">
-              <BtnTwitter />
+      <Navbar className="NavBarContent" light expand="md">
+        <NavbarBrand>
+          <Link className="linkTitleNavBar" to="/">
+            Victor Veyrier
+          </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link className="linkNavBar" to="/CV">
+                  CV
+                </Link>
+              </NavLink>
             </NavItem>
-            <NavItem className="IconNav">
-              <BtnLinkedin />
+            <NavItem>
+              <NavLink id="ContactMobil">
+                <Link className="linkNavBar" to="/Contact">
+                  Contact
+                </Link>
+              </NavLink>
             </NavItem>
-            <NavItem className="IconNav">
-              <BtnGithub />
+            <NavItem>
+              <NavLink id="ContactDestock"  className="linkNavBar">
+              <ContactDestock />
+              </NavLink>
             </NavItem>
-            <NavbarText id="TextNavBar">Développement Web</NavbarText>
-          </Collapse>
-        </Navbar>
-        <Switch>
-          <Route path="/CV">
-            <CV />
-          </Route>
-          <Route path="/Contact">
-            <ContactMobile />
-          </Route>
-          <Route exact path="/">
-            <Portfolio />
-          </Route>
-        </Switch>
-      </Router>
+            <NavItem>
+              <NavLink>
+                <a
+                  className="linkNavBar"
+                  href="https://github.com/CzerwonyV/CzerwonyV/files/4765663/VictorVeyrier.pdf"
+                  download
+                >
+                  <p className="linkNavBarPDFDestock">Télécharger mon CV</p>
+                  <p className="linkNavBarPDFMobile">CV en format PDF</p>
+                </a>
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavItem className="IconNav">
+            <BtnTwitter />
+          </NavItem>
+          <NavItem className="IconNav">
+            <BtnLinkedin />
+          </NavItem>
+          <NavItem className="IconNav">
+            <BtnGithub />
+          </NavItem>
+          <NavbarText id="TextNavBar">Développement Web</NavbarText>
+        </Collapse>
+      </Navbar>
     </div>
   );
 };
