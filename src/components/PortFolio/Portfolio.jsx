cee6110ab67ab72ee477bdb0109089d303ee7d53
CzerwonyV <victor.veyrier@icloud.com>
2020-07-10 23:13:57 +0200

    feat(portfolio): change actu text

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -14,9 +14,9 @@ const Portfolio = () => {
         <div className="ActuContent">
           <div className="ActuGauche">
             <BtnActu />
-            <p className="TextActu">Je suis disponible et très motivé pour un stage de 4 mois à partir du 3 août dans la région parisienne, dans la Marne (Châlons,Reims).</p>
-            <p className="TextActu">Projet 3 à la WildCodeSchool Tonton Sommelier. Réalisé du 25 Mai 2020 au 31 Juillet 2020. Equipe de 4 personnes. Fait avec ReactJS, ReactStrap, CSS3, NodeJs, ExpressJs. Le but est de réaliser une Application Web autour de la dégustation du vin avec la du client Tonton Sommelier.</p>
+            <p className="TextActu">Je suis à la recherche d'un stage de 4 mois à partir du 3 août dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
             <p className="TextActu" id="TextActuBottom">Je suis actuellement en Formation développeur Full Stack en 5 Mois à la Wild Code School de Reims.</p>
+            <p className="TextActu">Projet 3 à la WildCodeSchool Tonton Sommelier. Réalisé du 25 Mai 2020 au 31 Juillet 2020. Equipe de 4 personnes. Fait avec ReactJS, ReactStrap, CSS3, NodeJs, ExpressJs. Le but est de réaliser une Application Web autour de la dégustation du vin avec la du client Tonton Sommelier.</p>
           </div>
           <div className="ActuDroite">
             <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
