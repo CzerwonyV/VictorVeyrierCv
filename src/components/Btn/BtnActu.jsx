31fe867f8d947409aacc75d511a81519ef05f157
CzerwonyV <victor.veyrier@icloud.com>
2020-09-29 00:31:38 +0200

    feat(BtnActu): change img and url

--- src/components/Btn/BtnActu.jsx
+++ src/components/Btn/BtnActu.jsx
@@ -8,17 +8,17 @@ const BtnActu = () => {
       <div className="CercleBtn">
         <Button color="link" className="BtnWild">
           <a
-            href="https://www.wildcodeschool.com/fr-FR"
+            href="https://www.trott-app.com/"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener noreferrer" 
             className=""
           >
-            <img src="https://user-images.githubusercontent.com/57908921/85471152-54374000-b5b0-11ea-9950-ca2b981eabac.png" alt="BtnWild" className="ImgWild" />
+            <img src="https://user-images.githubusercontent.com/57908921/94474687-4821d680-01ce-11eb-8790-2bc5b74484ba.png" alt="BtnWild" className="ImgWild" />
           </a>
         </Button>
       </div>
       <div className="TitleActuContent">
-        <h4 className="TitleActu">Mon actu:</h4>
+        <h4 className="TitleActu">Mon actu :</h4>
       </div>
     </div>
   );
