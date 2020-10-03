b50eef6f4f3eaa2668a0ef00c8f16304be0c05f3
CzerwonyV <victor.veyrier@icloud.com>
2020-10-03 18:06:31 +0200

    feat(portfolio): add card

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import WildPostCard from "./SiteCard/WildPostCard";
-import BornInSpaceCard from "./SiteCard/BornInSpaceCard";
-import AroundTheWorldCard from "./SiteCard/AroundTheWorldCard";
+import Cards from "./SiteCard/Card";
 import TwitterTimeline from "react-twitter-embedded-timeline";
-import QRV from "./SiteCard/QRV";
-import BtnActu from "../Btn/BtnActu";
 import "./Portfolio.css";
 
 const Portfolio = () => {
@@ -13,7 +9,6 @@ const Portfolio = () => {
       <div className="PortFolioContent">
         <div className="ActuContent">
           <div className="ActuGauche">
-            <BtnActu />
             <p className="TextActu">Je suis à la recherche d'un emploi de développeur à partir du 27 novembre dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
             <p className="TextActu">Je suis actuellement en stage dans la start-up Trott depuis le 3 août et jusqu'aux 27 novembre.</p>
           </div>
@@ -21,11 +16,8 @@ const Portfolio = () => {
             <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
           </div>
         </div>
-        <div className="PortFolioContentCard">
-          <WildPostCard />
-          <AroundTheWorldCard />
-          <BornInSpaceCard />
-          <QRV />
+        <div>
+          <Cards/>
         </div>
       </div>
     </>
