7fb22dd7d0b01ae56bf34ef4f53fd367a272419e
CzerwonyV <victor.veyrier@icloud.com>
2020-10-03 18:04:17 +0200

    feat(api): add api and provider

---
+++ src/components/api/Provider.js
@@ -0,0 +1,30 @@
+export const Sites = [
+    {
+      "name": "WildPost",
+      "urlImg": "https://user-images.githubusercontent.com/57908921/85213516-d0871480-b35f-11ea-9b63-ad78b0245ba4.jpg",
+      "descritpion": "Projet 1, de la Wild Code School. Realisé du 09/03/2020 au 27/03/2020. En équipe de 4 personnes. Fait avec : HTML5, CSS3 et un peu de JS. Le but était de realiser un site statique d'un journal sur la WildCodeSchool.",
+      "urlSite": "https://czerwonyv.github.io/Wild-post/",
+      "urlGithub": "https://github.com/CzerwonyV/Wild-post",
+    },
+    {
+        "name": "Born In Space",
+        "urlImg": "https://user-images.githubusercontent.com/57908921/85213522-da107c80-b35f-11ea-8c1f-b15d746370a0.jpg",
+        "descritpion": "Projet 2, de la Wild Code School. Réalisé du 6 Avril 2020 au 15 Mai 2020. Equipe de 4 personnes. Fait avec JSReact, ReactStrap, CSS3. Le but était de réaliser une Application Web avec les API de la Nasa.",
+        "urlSite": "https://born-in-space.netlify.app/",
+        "urlGithub": "https://github.com/CzerwonyV/reims-0320-js-space-at-home",
+      },
+      {
+        "name": "Around The World",
+        "urlImg": "https://user-images.githubusercontent.com/57908921/85213518-d5e45f00-b35f-11ea-933d-2a06d7a259c5.png",
+        "descritpion": "Hackathon 1, de la Wild Code School. Réalisé du 29 Avril 2020 au 30 Avril 2020. Equipe de 3 personnes. Fait avec JSReact, ReactStrap, CSS3. Le but était de réaliser une Application Web en 32H sur une API de WebCam.",
+        "urlSite": "https://around-the-world-hackathon.netlify.app/",
+        "urlGithub": "https://github.com/CzerwonyV/reims-0320-js-around-the-world",
+      },
+      {
+        "name": "QR-V",
+        "urlImg": "https://user-images.githubusercontent.com/57908921/85213523-df6dc700-b35f-11ea-9cc8-4122adc67ece.png",
+        "descritpion": "Projet perso. Une application Web autour des QR Code afin de pouvoir en créer et en lire. Elle servira également à aider mes camarades sur les projets de la Wild Code School, qui utilise elle aussi les QR Code.",
+        "urlSite": "https://qrv.netlify.app/",
+        "urlGithub": "https://github.com/CzerwonyV/QR-V",
+      },
+  ]
