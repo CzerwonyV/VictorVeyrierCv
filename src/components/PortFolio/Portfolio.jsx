b6c89466b957956a44e48d3a5ff39c6638df1b39
CzerwonyV <victor.veyrier@icloud.com>
2020-10-13 18:14:49 +0200

    fix(portfolio): add css and text for safari

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -7,19 +7,22 @@ import "./Portfolio.css";
 const Portfolio = () => {
   return (
     <>
-      <NavBarCV />
-      <div className="PortFolioContent">
-        <div className="ActuContent">
-          <div className="ActuGauche">
-            <p className="TextActu">Je suis à la recherche d'un emploi de développeur à partir du 27 novembre dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
-            <p className="TextActu">Je suis actuellement en stage dans la start-up Trott depuis le 3 août et jusqu'aux 27 novembre.</p>
+      <p className="SafariOnlyP">Mon portfolio n'est pas disponible sur Safari pour l'instant</p>
+      <div className="SafariOnly">
+        <NavBarCV />
+        <div className="PortFolioContent">
+          <div className="ActuContent">
+            <div className="ActuGauche">
+              <p className="TextActu">Je suis à la recherche d'un emploi de développeur à partir du 27 novembre dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
+              <p className="TextActu">Je suis actuellement en stage dans la start-up Trott depuis le 3 août et jusqu'aux 27 novembre.</p>
+            </div>
+            <div className="ActuDroite">
+              <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
+            </div>
           </div>
-          <div className="ActuDroite">
-            <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
+          <div>
+            <Cards/>
           </div>
-        </div>
-        <div>
-          <Cards/>
         </div>
       </div>
     </>
