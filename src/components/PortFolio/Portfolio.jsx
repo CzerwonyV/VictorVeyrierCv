c3ae63473681ec4bec5dcc0e159aa5dd9d81a59f
Victor Veyrier <victor.veyrier@icloud.com>
2021-02-02 05:12:06 +0100

    fix(Portfolio): remove twitter timeline

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Cards from "./SiteCard/Card";
-import TwitterTimeline from "react-twitter-embedded-timeline";
 import NavBarCV from "../NavBar/NavBar";
 import "./Portfolio.css";
 
@@ -12,9 +11,6 @@ const Portfolio = () => {
         <div className="ActuContent">
           <div className="ActuGauche">
             <p className="TextActu">Je suis à la recherche d'un emploi de développeur dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
-          </div>
-          <div className="ActuDroite">
-            <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
           </div>
         </div>
           <Cards/>
