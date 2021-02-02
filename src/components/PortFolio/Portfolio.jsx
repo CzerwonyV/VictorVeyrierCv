37757466c9e9808f526abe09fc577ca18837f4f4
Victor Veyrier <victor.veyrier@icloud.com>
2021-02-02 05:07:04 +0100

    feat(Portfolio): remove stage and date of this

--- src/components/PortFolio/Portfolio.jsx
+++ src/components/PortFolio/Portfolio.jsx
@@ -11,8 +11,7 @@ const Portfolio = () => {
       <div className="PortFolioContent">
         <div className="ActuContent">
           <div className="ActuGauche">
-            <p className="TextActu">Je suis à la recherche d'un emploi de développeur à partir du 27 novembre dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
-            <p className="TextActu">Je suis actuellement en stage dans la start-up Trott depuis le 3 août et jusqu'aux 27 novembre.</p>
+            <p className="TextActu">Je suis à la recherche d'un emploi de développeur dans la région parisienne ou dans la Marne (Châlons, Reims). Je suis très motivé et aurai à cœur de m'investir dans le travail qui me sera confié.</p>
           </div>
           <div className="ActuDroite">
             <TwitterTimeline widgetId="TLContent" user="CzerwonyV" width="400" height="400" />
